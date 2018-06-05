277bf81b8eea42c184b0f4032a0611e338620e72
Zach Nolan <zach@uplinkconsulting.com>
2018-06-04 23:42:23 -0700

    react-router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from 'react-apollo'
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 import './reset.css'
 import './index.css'
 import Home from './screens/Home'
+import Admin from './screens/Admin'
 import registerServiceWorker from './registerServiceWorker'
 
 const client = new ApolloClient({
@@ -13,7 +15,22 @@ const client = new ApolloClient({
 
 const App = () => (
   <ApolloProvider client={client}>
-    <Home />
+    <BrowserRouter>
+      <div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/admin">Admin</Link>
+            </li>
+          </ul>
+        </div>
+        <Route exact path='/' component={Home} />
+        <Route path='/admin' component={Admin} />
+      </div>
+    </BrowserRouter>
   </ApolloProvider>
 )
 
